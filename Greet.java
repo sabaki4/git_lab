fe6684178d809aaf1c74bb2d9b908b0f33bed5bd
Saba Kia <skia6589@gmail.com>
2024-03-01 13:03:47 +0000

    new modification of good morning and how are you?

--- Greet.java
+++ Greet.java
@@ -1,6 +1,7 @@
-public class Greet{
-   public static void main(String[] args){
-        System.out.println("hello");
-        System.out.println("How are you?");  // ADDED
-    } 
+public class Greet {
+    public static void main(String[] args) {
+        String greeting = "Good morning";  // ADDED
+        System.out.println(greeting);  // MODIFIED
+        System.out.println("How are you?"); 
+    }
 }
